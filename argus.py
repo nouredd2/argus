67a9d726594b691a76a9a4908aa902a210824ed7
Mohammad <nouredd2@illinois.edu>
2017-12-15 16:30:13 -0600

    started add config options

--- argus.py
+++ argus.py
@@ -11,21 +11,63 @@ class Argus(Daemon):
         super(Argus, self).__init__(pidfile, stdin, stdout, stderr)
         self.config_file = config_file
         self.configured = False
+        self.output_file = 'argus.out'
+        self.metrics = set(['busy_workers',
+                            'bytes_per_request',
+                            'bytes_per_second',
+                            'cpu_load',
+                            'idle_workers',
+                            'requests_per_second',
+                            'total_accesses',
+                            'total_traffic',
+                            'uptime'])
 
-    def configure(self):
-        self.configured = True
 
     def flush_data(self):
         """
         Flush the collected data to an output file to be saved
         """
 
-    def read_config_file(self, config_file='argus.conf'):
+
+    def apply_config_option(self, o, v):
+        """
+        Apply the configuration option read from the config file
+        """
+        if o == 'output_file':
+            self.output_file = v
+        else:
+            sys.stderr.write("unrecognized configuration option %s\n" % o)
+            sys.exit(1)
+
+
+    def configure(self, config_file='argus.conf'):
         """
         Read the configuration for the argus daemon from the config file.
 
         Defaults to reading argus.conf in the current directory
         """
+        try:
+            f = file(config_file, 'r')
+            config_opts = f.readlines()
+            f.close()
+        except IOError:
+            config_opts = None
+
+        if config_opts is None:
+            sys.stderr.write("Cannot read config file %s\n" % config_file)
+            sys.exit(1)
+
+        opts = [x.strip() for x in config_opts]
+
+        for line in opts:
+            values = line.split()
+
+            option = values[0]
+            value = values[1]
+
+            self.apply_config_option(option, value)
+
+        self.configured = True
 
     # override the run method
     def run(self):
