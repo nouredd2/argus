e643f07ae251d7702109a4e0896940cbba8c3891
Mohammad <nouredd2@illinois.edu>
2017-12-14 18:51:28 -0600

    sketch of missing methods

--- argus.py
+++ argus.py
@@ -6,6 +6,27 @@ from apache_manager import ApacheManager
 
 
 class Argus(Daemon):
+    def __init__(self, pidfile, stdin='/dev/null', stdout='/dev/null',
+                 stderr='/dev/null/', config_file='argus.conf'):
+        super(Argus, self).__init__(pidfile, stdin, stdout, stderr)
+        self.config_file = config_file
+        self.configured = False
+
+    def configure(self):
+        self.configured = True
+
+    def flush_data(self):
+        """
+        Flush the collected data to an output file to be saved
+        """
+
+    def read_config_file(self, config_file='argus.conf'):
+        """
+        Read the configuration for the argus daemon from the config file.
+
+        Defaults to reading argus.conf in the current directory
+        """
+
     # override the run method
     def run(self):
         manager = ApacheManager()
@@ -30,8 +51,6 @@ if __name__ == "__main__":
             daemon.stop()
         elif 'restart' == sys.argv[1]:
             daemon.restart()
-        elif 'test':
-            daemon.run()
         else:
             print "Unknow command"
             sys.exit(2)
