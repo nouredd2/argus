fcd0ce6b85bfc388b0407c6eea5c7d73a3b04024
Mohammad <nouredd2@illinois.edu>
2017-12-15 18:38:28 -0600

    completed configuration and flushing options

--- argus.py
+++ argus.py
@@ -12,7 +12,7 @@ class Argus(Daemon):
         self.config_file = config_file
         self.configured = False
         self.output_file = 'argus.out'
-        self.metrics = set(['busy_workers',
+        self.metrics_names = set(['busy_workers',
                             'bytes_per_request',
                             'bytes_per_second',
                             'cpu_load',
@@ -21,12 +21,42 @@ class Argus(Daemon):
                             'total_accesses',
                             'total_traffic',
                             'uptime'])
+        self.sampling_rate = 2
+        self.flush_interval = 10
+        self.manager = None
+        self.last_flush = 0.0
+        self.write_header = True
 
 
-    def flush_data(self):
+    def flush_data(self, d):
         """
         Flush the collected data to an output file to be saved
         """
+        try:
+            f = open(self.output_file, 'a+')
+        except IOError:
+            sys.stderr.write("Error opening output file %s\n" %
+                             self.output_file)
+            f = None
+
+        if f is None:
+            self.stop()
+
+        if self.write_header:
+            f.write ("Timestamp ")
+            for title in self.metrics_names:
+                f.write(title + " ")
+            f.write("\n")
+            self.write_header = False
+
+        for ts,metrics in d.iteritems():
+            f.write("%lf " % ts)
+            for key,val in metrics.iteritems():
+                f.write("%lf " % val)
+            f.write("\n")
+
+        f.close()
+        self.last_flush = time.now()
 
 
     def apply_config_option(self, o, v):
@@ -35,9 +65,13 @@ class Argus(Daemon):
         """
         if o == 'output_file':
             self.output_file = v
+        elif o == 'sampling_rate':
+            self.sampling_rate = v
+        elif o == 'flush_interval':
+            self.flush_interval = v
         else:
             sys.stderr.write("unrecognized configuration option %s\n" % o)
-            sys.exit(1)
+            self.stop()
 
 
     def configure(self, config_file='argus.conf'):
@@ -55,7 +89,7 @@ class Argus(Daemon):
 
         if config_opts is None:
             sys.stderr.write("Cannot read config file %s\n" % config_file)
-            sys.exit(1)
+            self.stop()
 
         opts = [x.strip() for x in config_opts]
 
@@ -69,18 +103,36 @@ class Argus(Daemon):
 
         self.configured = True
 
+
+    def start(self):
+        self.configure()
+
+        super(Argus, self).start()
+
     # override the run method
     def run(self):
-        manager = ApacheManager()
+        if not self.configured:
+            sys.stderr.write("Argus monitoring agent is not configured.\n")
+            self.stop()
+
+        self.manager = ApacheManager()
         data = dict()
         while True:
-            manager.refresh()
-            metrics = manager.server_metrics
+            self.manager.refresh()
+            metrics = self.manager.server_metrics
 
             # we got the dictionary, now save everything
             timestamp = time.now()
             data[timestamp] = metrics
-            time.sleep(0.5)
+            time.sleep(1.0 / self.sampling_rate)
+
+            # check if should be flushing to file
+            if self.last_flush == 0.0:
+                self.last_flush = timestamp
+            else:
+                if (timestamp - self.last_flush) > self.flush_interval:
+                    self.flush_data(data)
+                    data.clear()
 
 
 if __name__ == "__main__":
